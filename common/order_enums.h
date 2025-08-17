3b75eb42e980bfa38fc3b545a5a66948dd8acaf0
Rishik Sahu <iamrishiksahu@gmail.com>
2025-08-17 07:50:54 +0000

    Added basic order enums

---
+++ common/order_enums.h
@@ -0,0 +1,39 @@
+namespace exch_ns
+{
+enum class Side
+{
+    Buy,
+    Sell,
+    Unknown
+};
+
+enum class ManulaOrderIndicator
+{
+    Manual,
+    Automatic
+};
+
+enum class OrderEventType
+{
+    New,
+    Trade,
+    Cancelled,
+    Rejected
+};
+
+enum class OrderStatus
+{
+    New,
+    PartFilled,
+    Filled,
+    Cancelled,
+    Rejected
+};
+
+enum class ErrorCode
+{
+    NoError,
+    CouldNotFindOrder,
+    OutOfMemory
+};
+} // namespace exch_ns
